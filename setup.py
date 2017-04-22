4cab7ba981ed2528cc65cf6cd2673a4fbc0d1d0a
frans <frans.fuerst+git@gmail.com>
2017-04-22 10:31:49 +0200

    change file structure

--- setup.py
+++ setup.py
@@ -85,7 +85,7 @@ setup(
     # installed, specify them here.  If using Python 2.6 or less, then these
     # have to be included in MANIFEST.in as well.
     package_data={
-        'picks': ['QTDark.stylesheet', 'picks.ui'],
+        'picks_viewer': ['QTDark.stylesheet', 'picks.ui'],
     },
 
     # Although 'package_data' is the preferred approach, in some case you may
